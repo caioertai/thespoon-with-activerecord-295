3b7a117b325fa7bf9b82b11a252621ccc930a639
Caio Andrade <caioertai@gmail.com>
2019-08-20 09:51:03 +1000

    Created restaurants index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'restaurants#index'
+
+  # Restaurants
+  # verb '/path', to: 'controller#action'
+
+  # 1. Preferred
+  # resources + only: or except:. Be mindful of what you're adding to the app!
+  resources :restaurants, only: [:index]
+
+  # 2. Yeah... that's ok...
+  # If you actually realize during the lifetime of your app that you need
+  # all 7 actions.
+  # resources :restaurants
+
+  # 3. Last resort
+  # IF and ONLY IF there's NO way to make the route work with resources
+  # hard code it with this syntax.
+  # Index
+  # get '/restaurants', to: 'restaurants#index'
+
+  # Show
+  # get '/restaurants/:id', to: 'restaurants#show'
+
+  # Destroy
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  # New
+  # get '/restaurants/new', to: 'restaurants#new'
+  # Create
+  # post '/restaurants', to: 'restaurants#create'
+
+  # Edit
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # Update
+  # put '/restaurants/:id', to: 'restaurants#update'
+  # patch '/restaurants/:id', to: 'restaurants#update'
 end
